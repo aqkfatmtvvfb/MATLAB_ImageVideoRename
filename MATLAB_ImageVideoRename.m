0b8056a537f485dd130108ce423cae7fc43e2ddc
WangYu <wangyu_1988@126.com>
2023-04-05 21:26:58 +0800

    改字符串连接的问题

--- MATLAB_ImageVideoRename.m
+++ MATLAB_ImageVideoRename.m
@@ -4,8 +4,8 @@ clear
 cfg_RenameVideoByModifiedDate=false;
 cfg_RenameNoExifPhotoByModifiedDate=false;
 
-% photo_dir = uigetdir('.', 'Pick Photo Directory');
-photo_dir ='D:\3 所有照片\MATLAB_ImageVideoRename';
+photo_dir = uigetdir('.', 'Pick Photo Directory');
+% photo_dir ='D:\3 所有照片\MATLAB_ImageVideoRename';
 cd(photo_dir);
 AllObject=dir();
 AllFile=AllObject([AllObject.isdir]==0);
@@ -15,7 +15,7 @@ for iFile=1:length(AllFile)
         info = imfinfo(AllFile(iFile).name);
         if isfield(info,'DateTime')
             photo_datetime = datetime(info.DateTime,'InputFormat','yyyy:MM:dd HH:mm:ss');
-            photo_datetime_str=string(photo_datetime,'yyyyMMdd_HHmmss');
+            photo_datetime_str=char(string(photo_datetime,'yyyyMMdd_HHmmss')); % char() change string 2 char to use [str1 str2]
             output_name=strcat('IMG_', photo_datetime_str);
             if strncmp(input_name,output_name,19) %if already renamed IMG_yyyyMMdd_HHmmss_i.jpg, pass
                 warning('%s already renamed',strcat(input_name,ext));
@@ -43,9 +43,11 @@ for iFile=1:length(AllFile)
                                 warning('%s can not be renamed',strcat(input_name,ext));
                             end
                             % SONY
-                            status=movefile(fullfile(photo_dir,strcat(input_name,'.ARW')),fullfile(photo_dir,[output_name '_' num2str(index) '.ARW']));
-                            if ~status
-                                warning('%s can not be renamed',strcat(input_name,'.ARW'));
+                            if exist(fullfile(photo_dir,[output_name '_' num2str(index) '.ARW']),'file')
+                                status=movefile(fullfile(photo_dir,strcat(input_name,'.ARW')),fullfile(photo_dir,[output_name '_' num2str(index) '.ARW']));
+                                if ~status
+                                    warning('%s can not be renamed',strcat(input_name,'.ARW'));
+                                end
                             end
                             break;
                         else
@@ -58,7 +60,7 @@ for iFile=1:length(AllFile)
             warning('%s has no exif information',strcat(input_name,ext));
         end
     elseif strcmpi(ext,'.mp4')
-        
+
     else
         continue;
     end
